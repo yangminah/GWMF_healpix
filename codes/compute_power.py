809d283d0bb0babe619bf98d79ac0c3acab32111
Minah Yang <lucia.yang@colorado.edu>
2023-10-29 20:24:11 -0400

    Add files via upload

--- codes/compute_power.py
+++ codes/compute_power.py
@@ -1,45 +1,35 @@
+"""
+Compute powers, iterating for each month.
+"""
+import os
+import argparse
 import numpy as np
 import xarray as xr
-import healpy as hp
 import intake
-import os
-import sys
-import time
-
-from compute_fluxes_hp import *
-import dask
 from dask.distributed import Client
-
-import ctypes
-from datetime import datetime
-from shutil import rmtree
-from compute_fluxes_hp import *
-
-# from plot import wavenumber2flat
-
-# import cartopy.crs as ccrs
-# import cartopy.feature as cf
-# import cmocean
-import matplotlib.pylab as plt
-import matplotlib as mpl
-import argparse
+from compute_fluxes_hp import trim_memory
 
 
-def wavenumber2flat(m, l, T):
-    if np.isscalar(m) == True:
+def wavenumber2flat(m, l, lmax):
+    """
+    Convert wavenumber (m,l) to its flattened index, lmax.
+    """
+    if np.isscalar(m):
         n = 0
         for i in range(m):
-            n += T + 1 - i
+            n += lmax + 1 - i
         n += l - m
         return n
-    else:
-        n = np.zeros(len(m), dtype=int)
-        for ii in range(len(m)):
-            n[ii] = wavenumber2flat(m[ii], l[ii], T)
-        return n
+    n = np.zeros(len(m), dtype=int)
+    for ii,mm in enumerate(m):
+        n[ii] = wavenumber2flat(mm, l[ii], lmax)
+    return n
 
 
 def main():
+    """
+    Compute powers from precomputed spherical harmonics coefficients.
+    """
     parser = argparse.ArgumentParser(description="Compute power")
 
     ## Arguments that define the model/data
@@ -71,17 +61,17 @@ def main():
     # Get indices of levels we're interested in.
     mean_heights = np.mean(ds_OG.zg.values, axis=1)
     height_inds = []
-    N_height = 6
+    n_height = 6
     for z in [6, 8, 12, 16, 20, 24]:
         height_inds.append((np.abs(mean_heights - z * 1000)).argmin())
 
     # Create an array of months written out in string form.
-    N_month = 60
+    n_month = 60
     months = {}
     year = 2020
     month = 2
     months[0] = f"{year}-{month:02d}"
-    for month_ind in range(1, N_month):
+    for month_ind in range(1, n_month):
         month += 1
         if month == 13:
             year += 1
@@ -110,19 +100,19 @@ def main():
         ds = ds.chunk(chunks={"nsp": ds.nsp.shape[0], "time": 4, "level_full": 1})
 
     # Loop over months and levels
-    # for month_ind in range(N_month):
+    # for month_ind in range(n_month):
     for month_ind in range(task_id, task_id + 5):  # (5x12)
         date = months[month_ind]
         print(date)
         filename = f"/work/bm1233/icon_for_ml/spherical/nextgems3/power/{var}/{l_max}/{date}.npy"
-        Cl = np.zeros((N_height, l_max + 1))
+        Cl = np.zeros((n_height, l_max + 1))
         if l_max == 214:
             with open(
                 f"/work/bm1233/icon_for_ml/spherical/nextgems3/power/{var}/71/{date}.npy",
                 "rb",
             ) as f:
                 Cl[:, :lstart1] = np.load(f)[:, :lstart1]
-        for level_ind in range(N_height):
+        for level_ind in range(n_height):
             height_ind = height_inds[level_ind]
             client.run(trim_memory)
             # Retrieve dataset at correct month & level
@@ -134,7 +124,8 @@ def main():
                 axis=0,
             )
 
-            # Get the remaining modes powers (for each l, m = 1, .., l; and double the magnitudes for m=-l,...,-1)
+            # Get the remaining modes powers (for each l, m = 1, .., l;
+            # and double the magnitudes for m=-l,...,-1)
             for l in range(lstart1, l_max + 1):
                 client.run(trim_memory)
                 Cl[level_ind, l] += 2 * np.mean(
